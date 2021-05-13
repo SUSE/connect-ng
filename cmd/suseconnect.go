6c8e5f485dcf480c19bc24b7230677706c6b888e
Darragh O'Reilly <doreilly@suse.com>
2021-05-13 14:04:18 +0100

    move flag parsing to init()

--- cmd/suseconnect.go
+++ cmd/suseconnect.go
@@ -11,28 +11,30 @@ import (
 var (
 	//go:embed usage.txt
 	usageText string
+	status    bool
 )
+
+func init() {
+	// display help like the ruby SUSEConnect
+	flag.Usage = func() {
+		fmt.Fprint(os.Stderr, usageText)
+	}
+
+	flag.BoolVar(&status, "status", false, "")
+	flag.BoolVar(&status, "s", false, "")
+
+	if len(os.Args) < 2 {
+		flag.Usage()
+		os.Exit(1)
+	}
+	flag.Parse()
+}
 
 func main() {
 	if os.Geteuid() != 0 {
 		fmt.Fprintln(os.Stderr, "Root privileges are required to register products and change software repositories.")
 		os.Exit(1)
 	}
-
-	var status bool
-	flag.BoolVar(&status, "status", false, "")
-	flag.BoolVar(&status, "s", false, "")
-
-	// display help like the ruby SUSEConnect
-	flag.Usage = func() {
-		fmt.Fprint(os.Stderr, usageText)
-	}
-	if len(os.Args) < 2 {
-		flag.Usage()
-		os.Exit(1)
-	}
-	flag.Parse()
-
 	if status {
 		fmt.Println(connect.GetProductStatuses("json"))
 		return
