ebab34f91bf9e4524d03a9fab5af2e1234e1ccff
Darragh O'Reilly <doreilly@suse.com>
2021-05-18 15:34:27 +0100

    update ruby example to free memory
    https://github.com/ffi/ffi/issues/467#issuecomment-159370223

--- ext/use-lib.rb
+++ ext/use-lib.rb
@@ -1,11 +1,20 @@
 require 'ffi'
 
+# see https://github.com/ffi/ffi/issues/467#issuecomment-159370223
+
+module Stdio
+  extend FFI::Library
+  ffi_lib FFI::Platform::LIBC
+  attach_function :free, [ :pointer ], :void
+end
+
 module SUSEConnect
   extend FFI::Library
   ffi_lib './libsuseconnect.so'
 
-  attach_function :getstatus, [:string], :string
+  attach_function :getstatus, [:string], :pointer
 end
 
-puts SUSEConnect.getstatus('json')
-
+p_out = SUSEConnect.getstatus('json')
+puts p_out.get_string(0)
+Stdio.free(p_out)
