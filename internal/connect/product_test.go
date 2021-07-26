70e15a2a152d567ad6d09f4c408f52c33bf501e0
Darragh O'Reilly <doreilly@suse.com>
2021-07-26 16:27:21 +0100

    Use custom UnmarshalJSON() for the Product struct
    Unlike SMT/RMT, SCC does not return an "available" field in it's
    JSON response, and the default UnmarshalJSON sets Available to
    false which is incorrect.
    
    This was worked around in a couple of places by checking if SCC was
    being used and overriding Availble to be true.
    
    This patch adds a custom UnmarshalJSON() so that the default value
    for Available is true, and the special checks are removed.

--- internal/connect/product_test.go
+++ internal/connect/product_test.go
@@ -1,6 +1,7 @@
 package connect
 
 import (
+	"encoding/json"
 	"testing"
 )
 
@@ -27,5 +28,47 @@ func TestEmptyProduct(t *testing.T) {
 	p3 := Product{Name: "sle-module-basesystem", Version: "15.2", Arch: "x86_64"}
 	if p3.isEmpty() {
 		t.Errorf("expected %v not to be empty", p3)
+	}
+}
+
+func TestUnmarshalJSONsmt(t *testing.T) {
+	jsn := `{"identifier": "product1", "available": true,
+               "extensions": [{"identifier": "extension1", "available": false}]}`
+	var p Product
+	if err := json.Unmarshal([]byte(jsn), &p); err != nil {
+		t.Errorf("Error unmarshalling: %s", err)
+	}
+	if p.Name != "product1" {
+		t.Errorf("Expected p.Name == product1, got %s", p.Name)
+	}
+	if !p.Available {
+		t.Error("Expected p.Aailable == true, got false")
+	}
+	if p.Extensions[0].Name != "extension1" {
+		t.Errorf("Expected p.Extensions[0].Name == product1, got %s", p.Extensions[0].Name)
+	}
+	if p.Extensions[0].Available {
+		t.Error("Expected p.Extensions[0].Available == false, got true")
+	}
+}
+
+func TestUnmarshalJSONscc(t *testing.T) {
+	jsn := `{"identifier": "product1",  "extensions": [{"identifier": "extension1"}]}`
+
+	var p Product
+	if err := json.Unmarshal([]byte(jsn), &p); err != nil {
+		t.Errorf("Error unmarshalling: %s", err)
+	}
+	if p.Name != "product1" {
+		t.Errorf("Expected p.Name == product1, got %s", p.Name)
+	}
+	if !p.Available {
+		t.Error("Expected p.Aailable == true, got false")
+	}
+	if p.Extensions[0].Name != "extension1" {
+		t.Errorf("Expected p.Extensions[0].Name == product1, got %s", p.Extensions[0].Name)
+	}
+	if !p.Extensions[0].Available {
+		t.Error("Expected p.Extensions[0].Available == true, got false")
 	}
 }
