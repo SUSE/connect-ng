1610135a26a191e631c2fb062f1d435998f3e2d9
Likhitha Priya <likhitha.priyad@suse.com>
2024-03-26 15:51:52 +0000

    Move zypper to its own package

--- internal/zypper/backup.go
+++ internal/zypper/backup.go
@@ -1,4 +1,4 @@
-package connect
+package zypper
 
 import (
 	_ "embed" //golint
@@ -7,6 +7,8 @@ import (
 	"path"
 	"strings"
 	"text/template"
+
+	"github.com/SUSE/connect-ng/internal/util"
 )
 
 var (
@@ -14,12 +16,12 @@ var (
 	restoreTemplate string
 )
 
-func createZypperTarball(tarballPath, root string, paths []string) error {
+func createTarball(tarballPath, root string, paths []string) error {
 	// tar reports an error if a file does not exist.
 	// So we have to check this before.
 	var existingPaths []string
 	for _, p := range paths {
-		if !fileExists(p) {
+		if !util.FileExists(p) {
 			continue
 		}
 		// remove leading "/" from paths to allow using them from different root
@@ -39,11 +41,11 @@ func createZypperTarball(tarballPath, root string, paths []string) error {
 	// tar doesn't seem to use -C root for output files
 	command := []string{"tar", "cz", "-C", root, "-f", tarballPathWithRoot}
 	command = append(command, existingPaths...)
-	_, err := execute(command, []int{0})
+	_, err := util.Execute(command, []int{0})
 
 	// tarball can contain sensitive data, so prevent read to non-root
 	// do it for sure even if tar failed as it can contain partial content
-	if fileExists(tarballPathWithRoot) {
+	if util.FileExists(tarballPathWithRoot) {
 		os.Chmod(tarballPathWithRoot, 0600)
 	}
 
@@ -53,7 +55,7 @@ func createZypperTarball(tarballPath, root string, paths []string) error {
 	return nil
 }
 
-func createZypperRestoreScript(scriptPath, tarballPath, root string, paths []string) error {
+func createRestoreScript(scriptPath, tarballPath, root string, paths []string) error {
 	var data struct {
 		Paths   []string
 		Tarball string
@@ -87,9 +89,9 @@ func createZypperRestoreScript(scriptPath, tarballPath, root string, paths []str
 	return nil
 }
 
-// ZypperBackup creates backup of zypper configuration files
-func ZypperBackup() error {
-	root := CFG.FsRoot
+// Backup creates backup of zypper configuration files
+func Backup() error {
+	root := zypperFilesystemRoot
 	if root == "" {
 		root = "/"
 	}
@@ -99,24 +101,24 @@ func ZypperBackup() error {
 		"/etc/zypp/services.d",
 	}
 	tarballPath := "/var/adm/backup/system-upgrade/repos.tar.gz"
-	if err := createZypperTarball(tarballPath, root, paths); err != nil {
+	if err := createTarball(tarballPath, root, paths); err != nil {
 		return err
 	}
 
 	scriptPath := "/var/adm/backup/system-upgrade/repos.sh"
-	if err := createZypperRestoreScript(scriptPath, tarballPath, root, paths); err != nil {
+	if err := createRestoreScript(scriptPath, tarballPath, root, paths); err != nil {
 		return err
 	}
 	return nil
 }
 
-// ZypperRestore restores zypper configuration from backup created by ZypperBackup
-func ZypperRestore() error {
-	root := CFG.FsRoot
+// Restore restores zypper configuration from backup created by ZypperBackup
+func Restore() error {
+	root := zypperFilesystemRoot
 	if root == "" {
 		root = "/"
 	}
-	_, err := execute([]string{"sh",
+	_, err := util.Execute([]string{"sh",
 		path.Join(root, "var/adm/backup/system-upgrade/repos.sh"),
 		root}, []int{0})
 	return err
--- internal/zypper/zypper_test.go
+++ internal/zypper/zypper_test.go
@@ -1,4 +1,4 @@
-package connect
+package zypper
 
 import (
 	"os"
