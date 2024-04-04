e3c7f5beccfe8bba2ac6b92eafe74a11d281d96e
Natnael Getahun <natnael.getahun@suse.com>
2024-04-04 11:07:53 +0200

    test collector run for arch

--- internal/collectors/architecture.go
+++ internal/collectors/architecture.go
@@ -1,8 +1,41 @@
 package collectors
 
+import (
+	"github.com/SUSE/connect-ng/internal/util"
+	"sync"
+)
+
 type ArchitectureInformation struct {
+	_json_key string
 }
 
-func (cpu *ArchitectureInformation) run(arch Architecture) (Result, error) {
-	return nil, nil
+var archCollectorInstance *ArchitectureInformation
+var singleton sync.Once
+
+func New() *ArchitectureInformation {
+	init := func() {
+		archCollectorInstance = &ArchitectureInformation{_json_key: "arch"}
+	}
+	singleton.Do(init)
+	return archCollectorInstance
+}
+
+func (a *ArchitectureInformation) get_key() string {
+	return a._json_key
+}
+
+func (a *ArchitectureInformation) run(arch Architecture) (Result, error) {
+	switch arch {
+	default:
+		return a.arch()
+	}
+}
+func (a *ArchitectureInformation) arch() (Result, error) {
+	output, err := util.Execute([]string{"uname", "-i"}, nil)
+	if err != nil {
+		return nil, err
+	}
+	m := Result{}
+	m[a.get_key()] = output
+	return m, nil
 }
--- internal/collectors/architecture_test.go
+++ internal/collectors/architecture_test.go
@@ -2,7 +2,39 @@ package collectors
 
 import (
 	"testing"
-	//	"github.com/stretchr/testify/assert"
+
+	"github.com/stretchr/testify/mock"
 )
 
-func TestArchitectureCollectorRun(t *testing.T) {}
+type MockArchitectureInformation struct {
+	mock.Mock
+}
+
+func (m *MockArchitectureInformation) run(arch Architecture) (Result, error) {
+	args := m.Called(arch)
+
+	return args.Get(0).(Result), args.Error(1)
+	// return m, nil
+}
+
+func TestArchitectureCollectorRun(t *testing.T) {
+	arch := "x86_64"
+	m := Result{"arch": arch}
+	testObj := new(MockArchitectureInformation)
+
+	// set up expectations
+	testObj.On("run", Architecture(arch)).Return(m, nil)
+
+	// call the code we are testing
+	testingFunc := func(t *testing.T, arch Architecture, colls []Collector) {
+		for _, col := range colls {
+			_, err := col.run(arch)
+			if err != nil {
+				t.Fail()
+			}
+		}
+	}
+	testingFunc(t, Architecture(arch), []Collector{testObj})
+	testObj.AssertExpectations(t)
+
+}
