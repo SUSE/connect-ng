223b9d70e9a665122f7a3d16ba089f53e84be230
Felix Schizlein <fschnizlein@suse.com>
2024-04-22 15:39:17 +0200

    collectors: Add cpu and socket detection
    Read values from lscpu for any architecture and remove s390x specific
    implementation since util-linux is available on all platforms now.
    
    Move from generic output to more parsable lscpu output (e.g. lscpu
    -pcpu,socket)

--- internal/collectors/cpu_test.go
+++ internal/collectors/cpu_test.go
@@ -10,16 +10,30 @@ import (
 
 func TestCPUCollectorRun(t *testing.T) {
 	assert := assert.New(t)
-	expected := Result{"cpus": 2, "sockets": 2}
+	expected := Result{"cpus": 8, "sockets": 2}
 	testObj := CPU{}
 
 	mockLscpu(t, "collectors/lscpu_x86_64.txt")
+
+	res, err := testObj.run(ARCHITECTURE_X86_64)
+
+	assert.NoError(err)
+	assert.Equal(expected, res, "Result mismatch")
+}
+
+func TestCPUCollectorRunInvalidCPU(t *testing.T) {
+	assert := assert.New(t)
+	expected := Result{"cpus": nil, "sockets": nil}
+	testObj := CPU{}
+
+	mockLscpu(t, "collectors/lscpu_x86_64_invalid.txt")
 
 	res, err := testObj.run(ARCHITECTURE_X86_64)
 	if err != nil {
 		t.Errorf("Something went wrong: %s", err)
 	}
 
+	assert.NoError(err)
 	assert.Equal(expected, res, "Result mismatch")
 }
 
@@ -28,7 +42,7 @@ func mockLscpu(t *testing.T, path string) {
 		actualCmd := strings.Join(cmd, " ")
 		testData := util.ReadTestFile(path, t)
 
-		assert.Equal(t, "lscpu", actualCmd, "Wrong command called")
+		assert.Equal(t, "lscpu -p=cpu,socket", actualCmd, "Wrong command called")
 
 		return testData, nil
 	}
