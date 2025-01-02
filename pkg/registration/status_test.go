91751a6655f9e586e885d517f82e5eac43f1d921
Felix Schnizlein <fschnizlein@suse.com>
2025-01-02 11:47:10 +0100

    Implement registration and status/keepalive in the public library
    The registration creates a new system in SCC and allows for performing
    the status/keepalive call to update the provided systemInformation.
    
    Note: Instead of historically named hwinfo, here everything is named
    systemInformation

---
+++ pkg/registration/status_test.go
@@ -0,0 +1,60 @@
+package registration_test
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+
+	"github.com/SUSE/connect-ng/pkg/registration"
+)
+
+const (
+	hostname = "test-hostname"
+)
+
+func TestStatusRegistered(t *testing.T) {
+	assert := assert.New(t)
+
+	conn, creds := mockConnectionWithCredentials()
+	login, password, _ := creds.Login()
+
+	// 204 No Content
+	conn.On("Do", mock.Anything).Return(204, []byte(""), nil).Run(checkAuthBySystemCredentials(t, login, password))
+
+	status, err := registration.Status(conn, hostname, nil)
+	assert.NoError(err)
+	assert.Equal(registration.Registered, status)
+}
+
+func TestStatusUnregistered(t *testing.T) {
+	assert := assert.New(t)
+
+	conn, _ := mockConnectionWithCredentials()
+
+	// 404 Not Found
+	conn.On("Do", mock.Anything).Return(404, []byte(""), errors.New("system not found"))
+
+	status, err := registration.Status(conn, hostname, nil)
+	assert.NoError(err)
+	assert.Equal(registration.Unregistered, status)
+}
+
+func TestStatusWithSystemInformation(t *testing.T) {
+	assert := assert.New(t)
+
+	payload := map[string]string{
+		"key": "value",
+	}
+
+	conn, _ := mockConnectionWithCredentials()
+
+	// 204 No Content
+	expected := string(fixture(t, "pkg/registration/status_with_system_information.json"))
+	conn.On("Do", mock.AnythingOfType("*http.Request")).Return(204, []byte(""), nil).Run(matchBody(t, expected))
+
+	status, err := registration.Status(conn, hostname, payload)
+	assert.NoError(err)
+	assert.Equal(registration.Registered, status)
+}
