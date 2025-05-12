4bc0289446cf1273f568eee441191246f36f026b
Felix Schnizlein <fschnizlein@suse.com>
2025-05-12 13:58:43 +0200

    Add label support to the library for the upcoming SUSEConnect port to the library

---
+++ pkg/labels/list_test.go
@@ -0,0 +1,27 @@
+package labels
+
+import (
+	"testing"
+
+	"github.com/SUSE/connect-ng/internal/testutil"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestListLabels(t *testing.T) {
+	assert := assert.New(t)
+
+	conn, _ := testutil.MockConnectionWithCredentials()
+
+	// 204 No Content
+	response := testutil.Fixture(t, "pkg/labels/list_labels_success.json")
+
+	conn.On("Do", mock.Anything).Return(response, nil).Run(testutil.MatchEmptyBody(t))
+
+	labels, err := ListLabels(conn)
+	assert.NoError(err)
+	assert.Len(labels, 3)
+	assert.Equal("label2", labels[1].Name)
+
+	conn.AssertExpectations(t)
+}
