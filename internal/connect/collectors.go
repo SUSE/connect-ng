a82952b8cf37dfd271c24c99af21bb0c6762f08e
sampsone <esampson@suse.com>
2025-08-26 15:22:14 -0500

    Add collector to get pci and lsmod data

--- internal/connect/collectors.go
+++ internal/connect/collectors.go
@@ -4,24 +4,25 @@ import (
 	"github.com/SUSE/connect-ng/internal/collectors"
 )
 
-// Collectors which are to be used when fetching the system information.
-var usedCollectors = []collectors.Collector{
-	collectors.CPU{},
-	collectors.Hostname{},
-	collectors.Memory{},
-	collectors.UUID{},
-	collectors.Virtualization{},
-	collectors.CloudProvider{},
-	collectors.Architecture{},
-	collectors.ContainerRuntime{},
-
-	// Optional collectors
-	collectors.Uname{},
-	collectors.SAP{},
-}
-
 // Fetch system information based on the available collectors for the system.
-func FetchSystemInformation() (collectors.Result, error) {
+func FetchSystemInformation(infoOnly bool) (collectors.Result, error) {
+	var usedCollectors = []collectors.Collector{
+		collectors.CPU{},
+		collectors.Hostname{},
+		collectors.Memory{},
+		collectors.UUID{},
+		collectors.Virtualization{},
+		collectors.CloudProvider{},
+		collectors.Architecture{},
+		collectors.ContainerRuntime{},
+		collectors.PCI{UpdateDataIDs: !infoOnly},
+		collectors.LSMOD{UpdateDataIDs: !infoOnly},
+
+		// Optional collectors
+		collectors.Uname{},
+		collectors.SAP{},
+	}
+
 	arch, err := collectors.DetectArchitecture()
 
 	if err != nil {
