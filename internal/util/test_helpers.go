1f064df5258fd0c52a7e7baee4f1ae8e8a9481e6
Fergal Mc Carthy <fmccarthy@suse.com>
2026-05-20 16:39:04 -0400

    util.MockExecutor helps with util.Execute mocking
    Leveraging the stretchr/testify/mock to provide a more capabale means
    for mocking util.Execute() calls.
    
    The MockExecutor.Setup() routine is used to replace util.Execute()
    with the MockExecutor's Execute() handler, and returns a teardown
    function that should be called with defer to restore the original
    Execute().
    
    The MockExecutor.Execute() routine is mock replacement for Execute()
    that checks for a pre-registered command line mocking and if found,
    handles it appropriately.
    
    The MockExecutor.OnExecute() routine allows registration of a command
    line to be mocked, returing a mock.Call that can be further used to
    manage the mocking of that call; for further details see the
    https://pkg.go.dev/github.com/stretchr/testify/mock#Call docs.
    
    The MockExecutor.OnExecuteReturn() return allows for registration of
    a command line and it's asssociated return values in a single command
    to simplify setting up mocking for a single command.
    
    Update ha.go and kubernetes_provider.go testing in internal/collectors
    to leverage the new MockExecutor for mocking util.Execute() calls.
    
    Also moved the systemdTestEnv support code that is shared by both of
    these collector implmentations into a separate test_helpers.go file,
    and added support for defining regular and dummy systemd services,
    and updated ha.go to use these new service definition helpers.
    
    Tweaked ha.go and ha_test.go to leverage constants for fixed string
    values and used full path for pacemakerd binary when calling it.

--- internal/util/test_helpers.go
+++ internal/util/test_helpers.go
@@ -5,6 +5,8 @@ import (
 	"path/filepath"
 	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/mock"
 )
 
 func ReadTestFile(name string, t *testing.T) []byte {
@@ -28,4 +30,91 @@ func TestContentMatches(t *testing.T, expected string, got string) {
 			"---"}
 		t.Errorf(strings.Join(message, "\n"), expected, got)
 	}
+}
+
+//
+// Helpers for mocking Execute()
+//
+
+type MockExecutor struct {
+	mock.Mock
+}
+
+func NewMockExecutor() *MockExecutor {
+	return &MockExecutor{}
+}
+
+// Setup replaces the standard Execute with a MockExecutor.Execute and
+// returns a teardown function that should be called as a deferred call
+// to restore the original Execute.
+//
+// Example:
+//
+//	mockExecutor := util.NewMockExecutor()
+//	someCall := mockExecutor.OnExecuteReturn(
+//	    []string{"some", "command", "line"],
+//	    []int{0},
+//	    []byte("output from running command"),
+//	    nil, // no error
+//	)
+//	... // define additional mock calls
+//	teardown := mockExecutor.Setup()
+//	defer teardown()
+//
+//	res, err := CodeThatCallsExecuteWithAboveCommandline()
+func (m *MockExecutor) Setup(t *testing.T) func() {
+	// retrieve the original Execute
+	origExecute := Execute
+
+	// define a teardown function
+	teardown := func() {
+		// restore the original Execute handler
+		Execute = origExecute
+
+		// check that expectations were met, such as calls being made
+		// or only
+		m.Mock.AssertExpectations(t)
+	}
+
+	// replace the original Execute with the Mock handler
+	Execute = m.Execute
+
+	return teardown
+}
+
+// Execute attempts to retrieve an associated pre-registered mocking
+// for the provided command line, and can be used to mock the Execute()
+// routine.
+func (m *MockExecutor) Execute(cmd []string, validExitCodes []int) (output []byte, err error) {
+	// attempt to retrieve any return values previously registered for this
+	// call signature; will panic if matching call signature not registered
+	args := m.Called(cmd, validExitCodes)
+
+	// extract output return value if provided
+	if args.Get(0) != nil {
+		output = args.Get(0).([]byte)
+	}
+
+	// extract error return value if provided
+	if args.Get(1) != nil {
+		err = args.Get(1).(error)
+	}
+
+	return
+}
+
+// compile time validation that MockExecutor.Execute matches ExecuteFunc
+var _ ExecuteFunc = (*MockExecutor)(nil).Execute
+
+// add an On("Execute", ...) mocking directive, returning a mock.Call that
+// can be manipulated with standard mock.Call methods, such as Once(), Maybe(),
+// etc. See https://pkg.go.dev/github.com/stretchr/testify/mock#Call for more
+// details on mock.Call.
+func (m *MockExecutor) OnExecute(cmd []string, validExitCodes []int) *mock.Call {
+	return m.On("Execute", cmd, validExitCodes)
+}
+
+// simplified wrapper to quickly setup a simple call with return values
+func (m *MockExecutor) OnExecuteReturn(cmd []string, validExitCodes []int, output []byte, err error) *mock.Call {
+	return m.OnExecute(cmd, validExitCodes).Return(output, err)
 }
